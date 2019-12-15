efda91c528016ca649c7005250d7b02762351f90
Cean Tolentino <cean.tolentino@gmail.com>
2019-12-15 14:14:16 -0800

    adding example.js

---
+++ example.js
@@ -0,0 +1,128 @@
+// People dropping off a forn (Action Creators)
+const createPolicy = (name, amount) => {
+   return {
+      // Action (a form in our analogy)
+      type: 'CREATE_POLICY',
+      payload: {
+         name: name,
+         amount: amount
+      }
+   };
+};
+
+const deletePolicy = name => {
+   return {
+      type: 'DELETE_POLICY',
+      payload: {
+         name: name
+      }
+   };
+};
+
+const createClaim = (
+   name,
+   amountOfMoneyToCollect
+) => {
+   return {
+      type: 'CREATE_CLAIM',
+      payload: {
+         name: name,
+         amountOfMoneyToCollect: amountOfMoneyToCollect
+      }
+   };
+};
+
+// Reducers (Departments!)
+const claimsHistory = (
+   oldListOfClaims = [],
+   action
+) => {
+   if (action.type === 'CREATE_CLAIM') {
+      // we care about this action (FORM!)
+      return [
+         ...oldListOfClaims,
+         action.payload
+      ];
+   }
+
+   // we don't care about this action (FORM!)
+   return oldListOfClaims;
+};
+
+const accounting = (
+   bagOfMoney = 100,
+   action
+) => {
+   if (action.type === 'CREATE_CLAIM') {
+      return (
+         bagOfMoney -
+         action.payload
+            .amountOfMoneyToCollect
+      );
+   } else if (
+      action.type === 'CREATE_POLICY'
+   ) {
+      return (
+         bagOfMoney +
+         action.payload.amount
+      );
+   }
+
+   return bagOfMoney;
+};
+
+const policies = (
+   listOfPolicies = [],
+   action
+) => {
+   if (
+      action.type === 'CREATE_POLICY'
+   ) {
+      return [
+         ...listOfPolicies,
+         action.payload.name
+      ];
+   } else if (
+      action.type === 'DELETE_POLICY'
+   ) {
+      return listOfPolicies.filter(
+         name =>
+            name !== action.payload.name
+      );
+   }
+
+   return listOfPolicies;
+};
+
+const {
+   createStore,
+   combineReducers
+} = Redux;
+
+const ourDepartments = combineReducers({
+   accounting: accounting,
+   claimsHistory: claimsHistory,
+   policies: policies
+});
+
+const store = createStore(
+   ourDepartments
+);
+
+store.dispatch(
+   createPolicy('Alex', 20)
+);
+store.dispatch(
+   createPolicy('Cean', 30)
+);
+store.dispatch(
+   createPolicy('Angela', 40)
+);
+
+store.dispatch(
+   createClaim('Cean', 130)
+);
+
+store.dispatch(deletePolicy('Alex'));
+
+console.log(store.getState());
